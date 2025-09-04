b91cd0300715b6b71acb40dd78b10f0c42bd88cb
EGS3D-Eafit <159959074+EGS3D-Eafit@users.noreply.github.com>
2025-09-04 11:38:40 -0500

    Order and Requirements update
    Changes in the order of the files and added the requirements.txt

--- LFminimal/urls.py
+++ LFminimal/urls.py
@@ -4,5 +4,10 @@ from lugares import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.welcome, name='welcome'),   # Página inicial con 3 opciones
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home, name='home'),
+    path('visitor/', views.visitor_login, name='visitor'),
 ]
